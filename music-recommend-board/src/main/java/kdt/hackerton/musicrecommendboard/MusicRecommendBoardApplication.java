dec9c853a887bc0201fb49cadd043ff9cde83e87
devrunner21 <devrunner21@gmail.com>
2021-09-23 11:05:32 +0900

    [INIT] 프로젝트를 생성하라.

---
+++ music-recommend-board/src/main/java/kdt/hackerton/musicrecommendboard/MusicRecommendBoardApplication.java
@@ -0,0 +1,13 @@
+package kdt.hackerton.musicrecommendboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRecommendBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicRecommendBoardApplication.class, args);
+    }
+
+}
---
+++ music-recommend-board/src/test/java/kdt/hackerton/musicrecommendboard/MusicRecommendBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kdt.hackerton.musicrecommendboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicRecommendBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
